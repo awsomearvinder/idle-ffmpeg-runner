086c6a8aaed5e29a5633c1eaa94679e3c2559a55
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2023-02-28 21:55:52 -0600

    refactor: move file stream loop outside of encode function.
    We also rename it to be more explicit, but that's not really that
    important. It's a lot cleaner now having a better seperation of concerns
    here.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use fs4::tokio::AsyncFileExt;
 use std::convert::identity;
 
-use tokio::{
-    fs::{self, DirEntry},
-    process, time,
-};
-use tokio_stream::{Stream, StreamExt};
+use tokio::{fs, process, time};
+use tokio_stream::StreamExt;
 
 mod activity;
 mod pausable_process;
@@ -31,8 +28,26 @@ async fn main() {
             })
             .filter_map(identity);
 
-        let videos = Box::pin(videos);
-        run_encode(videos, &settings).await;
+        let mut videos = Box::pin(videos);
+        while let Some(file) = videos.next().await {
+            println!("{}", file.file_name().to_str().unwrap());
+
+            // ignore non-videos
+            if mime_guess::from_path(file.path())
+                .first_or_octet_stream()
+                .type_()
+                != mime::VIDEO
+            {
+                continue;
+            }
+
+            // if we aren't exclusive just try the next file, this one is actively being used for something.
+            // e.g. recording.
+            if !try_exclusive(fs::File::open(file.path()).await.unwrap()).unwrap() {
+                continue;
+            }
+            run_encode_when_inactive(file.path(), &settings).await;
+        }
         time::sleep(time::Duration::from_secs(1)).await;
     }
 }
@@ -57,74 +72,56 @@ fn try_exclusive(file: fs::File) -> Result<bool, std::io::Error> {
     Ok(true)
 }
 
-async fn run_encode<T: Stream<Item = DirEntry> + Unpin>(mut videos: T, settings: &Settings) {
-    while let Some(file) = videos.next().await {
-        println!("{}", file.file_name().to_str().unwrap());
-        let mut output_path = settings.output_folder.join(file.file_name());
-        if !settings.output_file_extension.is_empty() {
-            output_path.set_extension(&settings.output_file_extension);
-        }
+async fn run_encode_when_inactive(input: std::path::PathBuf, settings: &Settings) {
+    let mut output_path = settings.output_folder.join(input.file_name().unwrap());
 
-        let f = fs::File::open(file.path()).await.unwrap();
+    if !settings.output_file_extension.is_empty() {
+        output_path.set_extension(&settings.output_file_extension);
+    }
+    let output_path = output_path;
+    let child = process::Command::new("ffmpeg.exe")
+        .arg("-y")
+        .arg("-xerror")
+        .arg("-nostdin")
+        .arg("-i")
+        .arg(&input)
+        .args(shell_words::split(&settings.ffmpeg_flags).expect("failed to parse ffmpeg flags"))
+        .arg(settings.output_folder.join(output_path))
+        .spawn()
+        .unwrap();
 
-        if mime_guess::from_path(file.path())
-            .first_or_octet_stream()
-            .type_()
-            != mime::VIDEO
-        {
-            continue;
-        }
-
-        // if we aren't exclusive just try the next file, this one is actively being used for something.
-        // e.g. recording.
-        if !try_exclusive(f).unwrap() {
-            continue;
-        }
-
-        let child = process::Command::new("ffmpeg.exe")
-            .arg("-y")
-            .arg("-xerror")
-            .arg("-nostdin")
-            .arg("-i")
-            .arg(&file.path())
-            .args(shell_words::split(&settings.ffmpeg_flags).expect("failed to parse ffmpeg flags"))
-            .arg(&settings.output_folder.join(output_path))
-            .spawn()
-            .unwrap();
-
-        let mut proc = PausableProcess::new(child);
-        loop {
-            // race user input and ffmpeg
-            // if user input finishes first, pause ffmpeg and wait for the user to be active.
-            tokio::select! {
-                _ = activity::get_input() => {
-                    proc.pause().unwrap();
-                    wait_until_active(time::Duration::from_secs(settings.wait_time)).await;
-                    proc.unpause().unwrap();
-                }
-                status = proc.wait() => {
-                    match status {
-                        // While our file removal stuff is going we can start on the next batch.
-                        Ok(s) if s.success() => tokio::task::spawn(async move {
-                            let f = tokio::fs::File::open(file.path()).await.unwrap();
-
-                            // wait for us to be able to get an exclusive lock.
-                            // note: we immediately unlock and then try to delete it.
-                            // It would be ideal if we could lock and delete so we know no other
-                            // process is using it, but whatever. We do it this way just to attempt
-                            // to make sure no one else is using the file for whatever reason.
-                            let _ = wait_until_exclusive(f).await.unwrap();
-
-                            // *try* to delete the file.
-                            // ignore errors
-                            let _ = fs::remove_file(file.path()).await;
-                        }).await.unwrap(),
-                        _ => ()
-                    }
-                    println!("finished! {}", status.unwrap());
-                    break;
-                },
+    let mut proc = PausableProcess::new(child);
+    loop {
+        // race user input and ffmpeg
+        // if user input finishes first, pause ffmpeg and wait for the user to be active.
+        tokio::select! {
+            _ = activity::get_input() => {
+                proc.pause().unwrap();
+                wait_until_active(time::Duration::from_secs(settings.wait_time)).await;
+                proc.unpause().unwrap();
             }
+            status = proc.wait() => {
+                match status {
+                    // While our file removal stuff is going we can start on the next batch.
+                    Ok(s) if s.success() => tokio::task::spawn(async move {
+                        let f = tokio::fs::File::open(&input).await.unwrap();
+
+                        // wait for us to be able to get an exclusive lock.
+                        // note: we immediately unlock and then try to delete it.
+                        // It would be ideal if we could lock and delete so we know no other
+                        // process is using it, but whatever. We do it this way just to attempt
+                        // to make sure no one else is using the file for whatever reason.
+                        let _ = wait_until_exclusive(f).await.unwrap();
+
+                        // *try* to delete the file.
+                        // ignore errors
+                        let _ = fs::remove_file(&input).await;
+                    }).await.unwrap(),
+                    _ => ()
+                }
+                println!("finished! {}", status.unwrap());
+                break;
+            },
         }
     }
 }
